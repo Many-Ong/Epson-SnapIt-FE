fea33f014a4622cc530ca41e2b016170d0500782
ongheong <pudding4271@gmail.com>
2024-06-20 22:27:40 +0900

    ✨ Create a CameraScreen and set the camera lens direction to the front

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,129 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:ui';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras(); 
+  final firstCamera = cameras.first; 
+
+  runApp(MyApp(camera: firstCamera)); 
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final CameraDescription camera;
 
-  // This widget is the root of your application.
+  const MyApp({required this.camera});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData.dark(),
+      home: CameraScreen(camera: camera), 
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class CameraScreen extends StatefulWidget { 
+  final CameraDescription camera;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  const CameraScreen({required this.camera});
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+} //확인
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _CameraScreenState extends State<CameraScreen> {
+  CameraController? _controller; 
+  late Future<void> _initializeControllerFuture;
+  List<CameraDescription> cameras = [];
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void initState() { // 초기화 작업
+    super.initState();
+    initCamera();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Future<void> initCamera() async {
+    cameras = await availableCameras(); 
+    CameraDescription frontCamera = cameras.firstWhere( // 전면 카메라를 찾음
+      (camera) => camera.lensDirection == CameraLensDirection.front,
+      orElse: () => cameras.first,
+    ); // 전면 카메라 없으면 후면 카메라 사용
+    _controller = CameraController(frontCamera, 
+    ResolutionPreset.medium); // 카메라 컨트롤러를 초기화
+    _initializeControllerFuture = _controller!.initialize().then((_) {
+      if (!mounted) return;
+      setState(() {}); // UI 업데이트
+    }). catchError((error) {
+      print('카메라 초기화 중에 에러가 발생했습니다: $error');
+    }); // 
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    await _controller!.initialize();
+
+    if (!mounted) return;
+    setState(() {}); // UI 상태 업데이트
   }
 
   @override
+  void dispose() { // 카메라 사용이 끝나면 controller를 dispose해야 함 
+    _controller?.dispose(); 
+    super.dispose();
+  }
+
+ @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      appBar: AppBar(title: const Text('Custom Camera')),
+      body: FutureBuilder<void>( // FutureBuilder는 Future의 결과에 따라 UI를 업데이트
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Stack(
+              children: <Widget>[
+                CameraPreview(_controller!),
+                _buildOverlay(),
+              ],
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller!.takePicture();
+            if (!mounted) return;
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(imagePath: image.path),
+              ),
+            );
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: Icon(Icons.camera_alt),
+      ),
+    );
+  }
+
+  Widget _buildOverlay() {
+    return Align(
+      alignment: Alignment.center,
+      child: Image.asset('assets/camera_test.png'), // 카메라 위에 겹쳐질 이미지
     );
   }
 }
+
+Widget DisplayPictureScreen({required String imagePath}) { // 사진을 보여주는 화면
+  return Scaffold(
+    appBar: AppBar(title: Text('Display the Picture')),
+    body: Image.file(File(imagePath)),
+  );
+}
+
+
+
