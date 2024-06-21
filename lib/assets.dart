3789b3dd5cf0e2516728cef6f6d740901bd8362b
ongheong <87983309+ongheong@users.noreply.github.com>
2024-06-22 05:43:48 +0900

    Feature/4 images in one (#13)
    * 🍱 Add 3 more Image assets and named "Image1_{num}.png"
    
    * 🔨 Add overlayImages list to manage assets
    
    * ✨ Merge 4 overlayed images by one image
    
    * 🚩 Update print_screen to make size flexible

--- lib/assets.dart
+++ lib/assets.dart
@@ -1,3 +1,8 @@
 class Assets {
-  static const String overlayImage1 = 'assets/camera_test.png';
+  static const List<String> overlayImages = [
+    'assets/Image1_1.png',
+    'assets/Image1_2.png',
+    'assets/Image1_3.png',
+    'assets/Image1_4.png',
+  ];
 }
--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -7,7 +7,7 @@ import 'package:flutter/services.dart';
 import 'package:image/image.dart' as img;
 import 'package:snapit/screens/display_picture_screen.dart';
 import 'package:snapit/assets.dart';
-import 'print_screen.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CameraScreen extends StatefulWidget { 
   final CameraDescription camera; //
@@ -21,6 +21,8 @@ class _CameraScreenState extends State<CameraScreen> {
   CameraController? _controller; 
   late Future<void> _initializeControllerFuture;
   List<CameraDescription> cameras = [];
+  int pictureCount = 0; //촬영된 사진의 수
+  List<String> takePictures = []; //촬영된 사진 경로 목록
 
   @override
   void initState() { // 초기화 작업
@@ -36,15 +38,15 @@ class _CameraScreenState extends State<CameraScreen> {
     ); // 전면 카메라 없으면 후면 카메라 사용
     _controller = CameraController(frontCamera, 
     ResolutionPreset.medium); // 카메라 컨트롤러를 초기화
-    _initializeControllerFuture = _controller!.initialize().then((_) {
+    _initializeControllerFuture = _controller!.initialize().then((_) async {
       if (!mounted) return;
+      // 카메라 초기화가 완료되면 플래시 모드를 off로 설정
+      await _controller!.setFlashMode(FlashMode.off);
+
       setState(() {}); // UI 업데이트
     }). catchError((error) {
       print('카메라 초기화 중에 에러가 발생했습니다: $error');
     }); // 
-
-    if (!mounted) return;
-    setState(() {}); // UI 상태 업데이트
   }
 
   @override
@@ -73,7 +75,7 @@ class _CameraScreenState extends State<CameraScreen> {
                   child: AspectRatio(
                     aspectRatio: 4 / 3,
                     child: flutter.Image.asset(
-                      Assets.overlayImage1,
+                      Assets.overlayImages[pictureCount],
                       fit: BoxFit.contain, // Maintain the aspect ratio of the overlay image
                     ),
                   ),
@@ -88,21 +90,49 @@ class _CameraScreenState extends State<CameraScreen> {
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           try {
-            await _initializeControllerFuture;
-            final image = await _controller!.takePicture();
-            String mergedImagePath = await mergeImage(image, Assets.overlayImage1);
+            if (pictureCount < 4) {
+              await _initializeControllerFuture;
+              final XFile image = await _controller!.takePicture();
+              String overlayImagePath = await mergeImage(image, Assets.overlayImages[pictureCount]);
+              takePictures.add(overlayImagePath); // 이미지 경로를 목록에 추가
+              pictureCount++; // 촬영된 사진 수 증가
 
-            if (!mounted) return;
-
-            if (mergedImagePath.isNotEmpty) {
-              await Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => DisplayPictureScreen(imagePath: mergedImagePath, context: context),
-                ),
-              );
-            } else {
-              print('이미지 합성에 실패하여 파일이 저장되지 않았습니다.');
+              // 4장의 이미지가 모두 촬영되면 이미지 합성
+              if (pictureCount == 4) {
+                String mergedImagePath = await mergeFourImages(takePictures);
+                await Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => DisplayPictureScreen(imagePath: mergedImagePath, context: context),
+                  ),
+                );
+                pictureCount = 0; // 촬영된 사진 수 초기화
+                takePictures.clear(); // 촬영된 사진 경로 목록 초기화
+              }
             }
+            // await _initializeControllerFuture;
+            // final image = await _controller!.takePicture();
+            // String imagePath = image.path;
+            // takePictures.add(imagePath); // 이미지 경로를 목록에 추가
+            // String mergedImagePath = await mergeImage(image, Assets.overlayImages[pictureCount]); 
+            // pictureCount++; // 촬영된 사진 수 증가
+
+            // if (!mounted) return;
+
+            // if (merge) { 
+            //   String mergedFourImagePath = await mergeFourImages(takePictures);
+
+            //   if (pictureCount == 4){
+            //     await Navigator.of(context).push(
+            //       MaterialPageRoute(
+            //         builder: (context) => DisplayPictureScreen(imagePath: mergedImagePath, context: context),
+            //       ),
+            //     );
+            //     pictureCount = 0; //촬영된 사진 수 초기화
+            //     takePictures = []; //촬영된 사진 경로 목록 초기화
+            //   }
+            // } else {
+            //   print('이미지 합성에 실패하여 파일이 저장되지 않았습니다.');
+            // }
           } catch (e) {
             print(e);
           }
@@ -158,7 +188,32 @@ class _CameraScreenState extends State<CameraScreen> {
     return newImageFile.path;
   }
 
-  Widget _buildOverlay() {
-    return flutter.Image.asset(Assets.overlayImage1);
+  Future<String> mergeFourImages(List<String> imagePaths) async{
+    List<img.Image> images = [];
+
+    // 이미지 경로에서 이미지를 읽어들임
+    for (String path in imagePaths) {
+      img.Image image = img.decodeImage(File(path).readAsBytesSync())!;
+      images.add(image);
+    }
+    // 합설할 이미지의 너비와 높이 계산
+    int width = images[0].width;
+    int height = images.fold(0, (prev, element) => prev + element.height);
+
+    // 합성된 이미지를 저장할 새 이미지 생성
+    img.Image mergedFourImage = img.Image(width, height);
+
+    // 각 이미지를 적절한 위치에 복사
+    int offsetY = 0;
+    for (img.Image image in images) {
+      img.copyInto(mergedFourImage, image, dstY: offsetY);
+      offsetY += image.height;
+    }
+
+    // 합성된 이미지를 저장 후 리턴
+    Directory dic = await getApplicationDocumentsDirectory();
+    String filename = '${dic.path}/merged_${DateTime.now()}.png';
+    File(filename).writeAsBytesSync(img.encodePng(mergedFourImage));
+    return filename;
   }
 }
