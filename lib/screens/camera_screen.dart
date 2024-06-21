ebc148c1b367f7acad41fbf40ea129a3a01b6e36
Dayeon Yoo <udayeon@naver.com>
2024-06-21 19:47:32 +0900

    ✨ Api connect (#2)
    * 🔧 Update info.plist
    
    * 🔧 Add the http package to your pubspec.yaml file
    
    * 🔧 Update pubspec.lock file
    
    * ✨ Add api connect example
    
    * 💥 Split the screens

---
+++ lib/screens/camera_screen.dart
@@ -0,0 +1,106 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  CameraController? _controller;
+  late Future<void> _initializeControllerFuture;
+  List<CameraDescription> cameras = [];
+
+  @override
+  void initState() {
+    super.initState();
+    initCamera();
+  }
+
+  Future<void> initCamera() async {
+    cameras = await availableCameras();
+    CameraDescription frontCamera = cameras.firstWhere(
+      (camera) => camera.lensDirection == CameraLensDirection.front,
+      orElse: () => cameras.first,
+    );
+    _controller = CameraController(frontCamera, ResolutionPreset.medium);
+    _initializeControllerFuture = _controller!.initialize().then((_) {
+      if (!mounted) return;
+      setState(() {});
+    }).catchError((error) {
+      print('Error initializing camera: $error');
+    });
+
+    await _controller!.initialize();
+
+    if (!mounted) return;
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Custom Camera')),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Stack(
+              children: <Widget>[
+                CameraPreview(_controller!),
+                _buildOverlay(),
+              ],
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller!.takePicture();
+            if (!mounted) return;
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(imagePath: image.path),
+              ),
+            );
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: Icon(Icons.camera_alt),
+      ),
+    );
+  }
+
+  Widget _buildOverlay() {
+    return Align(
+      alignment: Alignment.center,
+      child: Image.asset('assets/camera_test.png'),
+    );
+  }
+}
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Display the Picture')),
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
