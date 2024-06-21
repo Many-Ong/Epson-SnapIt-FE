1c6800af5361c702a9679a28c26f6e6f82a5c373
manyyeon <udayeon@naver.com>
2024-06-22 04:51:05 +0900

    ✨ Add home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import '../services/api_service.dart';
 import 'camera_screen.dart';
-import 'package:camera/camera.dart';
 
 class HomeScreen extends StatelessWidget {
   final CameraDescription camera;
@@ -13,44 +12,38 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Screen')),
+      backgroundColor: Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  final data = await apiService.fetchData('user');
-                  print(data);
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: const Text('User Data'),
-                      content: Text(data.toString()),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () => Navigator.of(context).pop(),
-                          child: const Text('OK'),
-                        ),
-                      ],
-                    ),
+            const Spacer(),
+            Image.asset('assets/logo.png', width: 300), // Display the logo image
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 60),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CameraScreen(camera: camera)),
                   );
-                } catch (e) {
-                  print('Error fetching user data: $e');
-                }
-              },
-              child: const Text('Fetch User Data'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CameraScreen(camera: camera)),
-                );
-              },
-              child: const Text('Go to Camera'),
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+                child: const Text(
+                  'START',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
