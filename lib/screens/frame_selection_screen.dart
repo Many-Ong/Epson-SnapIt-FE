9cc4664239f7a090024d9645288ed738401b50e7
Dayeon Yoo <udayeon@naver.com>
2024-06-22 20:56:06 +0900

    ✨ Add frame-select feature (#16)

---
+++ lib/screens/frame_selection_screen.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'camera_screen.dart';
+import 'package:camera/camera.dart';
+
+class FrameSelectionScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  FrameSelectionScreen({super.key, required this.camera});
+
+  @override
+  _FrameSelectionScreenState createState() => _FrameSelectionScreenState();
+}
+
+class _FrameSelectionScreenState extends State<FrameSelectionScreen> {
+  final List<Color> frameColors = [
+    Colors.red,
+    Colors.pink,
+    Colors.purple,
+    Colors.lightBlue,
+    Colors.blue,
+    Colors.yellow,
+    Colors.orange,
+    Colors.blue.shade400,
+    Colors.green.shade200,
+    Colors.teal,
+    Colors.brown,
+    Colors.grey,
+    Colors.black,
+    Colors.white,
+  ];
+
+  Color selectedFrameColor = Colors.blue; // Default color
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Frame Selection'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+          child: 
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Text(
+                'Choose your favorite Frame',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+                ),
+                SizedBox(height: 10),
+                Center(
+                child: CustomPaint(
+                size: Size(120, 320),
+                painter: FramePainter(selectedFrameColor),
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Wrap(
+              spacing: 8.0,
+              runSpacing: 16.0,
+              children: frameColors.map((color) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedFrameColor = color;
+                    });
+                  },
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: color,
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: selectedFrameColor == color ? Colors.black : Colors.transparent,
+                        width: 4,
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CameraScreen(
+                      camera: widget.camera,
+                      frameColor: selectedFrameColor,
+                    ),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+              child: const Text(
+                'Ready To SHOOT!',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      ),
+    );
+  }
+}
+
+class FramePainter extends CustomPainter {
+  final Color frameColor;
+
+  FramePainter(this.frameColor);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = frameColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 6;
+
+    final double gap = 2;
+    final double height = (size.height - 5 * gap) / 4;
+
+    for (int i = 0; i < 4; i++) {
+      final double top = i * (height + gap) + gap;
+      final rect = Rect.fromLTWH(gap, top, size.width - 2 * gap, height);
+      canvas.drawRect(rect, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return oldDelegate is FramePainter && oldDelegate.frameColor != frameColor;
+  }
+}
